a26afc2d3717e7ff344c9d9d683fe42377e59a9b
Andre Colaris <80080964+AndreTheDeveloper@users.noreply.github.com>
2023-03-07 22:39:54 -0700

    added tilt when jumping and falling

--- narwhal-run/Assets/Scripts/NarwhalRunScripts/MobileController.cs
+++ narwhal-run/Assets/Scripts/NarwhalRunScripts/MobileController.cs
@@ -1,33 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Threading;
 
 public class MobileController : MonoBehaviour
 {
-    private bool isJumping = false;
+   private bool isJumping = false;
+    private bool reachedHighestPoint = false;
     private Vector2 startingPosition;
     public float jumpHeight = 5f;
     public float jumpDuration = 0.5f;
-    public float jumpTimer = 0f;
+    private float jumpTimer = 0f;
+    private float tiltAngle = 0f;
+    private Rigidbody2D rb;
+    private float timeInAir = 0f;
 
     void Start()
     {
         startingPosition = transform.position;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && !isJumping)
         {
+            timeInAir = 0;
             isJumping = true;
             jumpTimer = 0f;
+            tiltAngle = 45f;
         }
 
         if (isJumping)
         {
             float normalizedTime = jumpTimer / jumpDuration;
-            float jumpForce = Mathf.Sin(normalizedTime * Mathf.PI) * jumpHeight;
-            transform.position = startingPosition + Vector2.up * jumpForce;
+            float jumpPosition = Mathf.Sin(normalizedTime * Mathf.PI) * jumpHeight;
+            transform.position = startingPosition + Vector2.up * jumpPosition;
+            
+            timeInAir += Time.deltaTime;
+
+            if(timeInAir >= 1.251) {
+                Debug.Log(timeInAir);
+                tiltAngle = -45;
+            } else {
+                tiltAngle = 45;
+            }
+
+            transform.rotation = Quaternion.Euler(0, 0, tiltAngle);
 
             jumpTimer += Time.deltaTime;
 
@@ -35,6 +55,7 @@ public class MobileController : MonoBehaviour
             {
                 isJumping = false;
                 transform.position = startingPosition;
+                transform.rotation = Quaternion.identity;
             }
         }
     }
